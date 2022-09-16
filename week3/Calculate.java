7088a67eafb927fc20e1206285e97500b4dba57f
Brandon Price <43038187+bpricemg@users.noreply.github.com>
2022-09-16 15:48:28 +0000

    Updated 9/16

---
+++ week3/Calculate.java
@@ -0,0 +1,14 @@
+/**
+ * Sample all of the native mathematical operations
+ */
+class Calculate {
+    public static void main(String[] args) {
+        int first = 5;
+        int second = 2;
+
+        // Addition
+        int sum = first + second;
+        System.out.println(first + " plus " + second + " equals " + sum);
+    
+    }
+}
