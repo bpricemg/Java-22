087f97a42ccee44b415f2a738c103fc6e7a3d996
Brandon Price <43038187+bpricemg@users.noreply.github.com>
2022-09-19 15:49:40 +0000

    Updated 9/19

--- week3/Calculate.java
+++ week3/Calculate.java
@@ -7,8 +7,30 @@ class Calculate {
         int second = 2;
 
         // Addition
+        // Note**** An int plus and int is an int
+        // An int plus a double is a double
+        // A double plu a double is a double
         int sum = first + second;
         System.out.println(first + " plus " + second + " equals " + sum);
-    
+
+        // Subtraction
+        int difference = first - second;
+        System.out.println(first + " minus " + second + " equals " + difference);
+
+        // Multiplication
+        int product = first * second;
+        System.out.println(first + " times " + second + " equals " + product);
+
+        // Division
+        // Note* an integer divided by an integer is an integer!!! This is floor
+        // division
+        // If we need a decimal, at least one needs to be an double
+        double quotient = first / (double) second;
+        System.out.println(first + " divided by " + second + " equals " + quotient);
+
+        // Mod or Remainder
+        int remainder = first % second;
+        System.out.println("The remainder when " + first + " is divided by " + second + " is " + remainder);
+
     }
 }
---
+++ week3/Fahrenheit.java
@@ -0,0 +1,14 @@
+// Converts Fahrenheit to Celcius
+class Fahrenheit {
+    public static void main(String[] args) {
+        // Starting F value
+        double f = 70;
+
+        // Convert with formula 
+        // Subtract 32 then multiply by 5 / 9
+        double c = (f - 32) * (5 / 9.0); 
+
+        // Print the C value
+        System.out.println("The Celsius equivalent to " + f + " is " + c + ".");
+    }
+}
