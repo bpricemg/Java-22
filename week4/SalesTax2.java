1f0854906cb2744f7c256576ba0b62654d6b5325
Brandon Price <43038187+bpricemg@users.noreply.github.com>
2022-09-29 15:07:35 +0000

    Updated 9/28

--- week4/SalesTax2.java
+++ week4/SalesTax2.java
@@ -2,11 +2,11 @@
 
 import java.util.Scanner;
 
-class SalesTax {
+class SalesTax2 {
     public static void main(String[] args) {
         // Start a scanner
         Scanner input = new Scanner(System.in);
-        
+
         // Ask user for subtotal of taxable items
         System.out.print("Subtotal: $");
         double subtotal = input.nextDouble();
@@ -15,8 +15,11 @@ class SalesTax {
         double tax = subtotal * .0625;
         double total = subtotal + tax;
 
+        // Round the total to dollars and cents
+        total = Math.round(total * 100) / 100.0;
+
         // Print the total
         System.out.println("The total with MA sales tax is $" + total);
     }
-}
+
 
